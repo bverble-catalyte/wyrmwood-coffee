bdd950ed775f898ab360e29f8e9644678ad0379d
Sabah <asabah@catalyte.io>
2026-08-21 14:08:22 -0500

    Add GET /vendors endpoint

--- src/wyrmwood_coffee/routers/vendors.py
+++ src/wyrmwood_coffee/routers/vendors.py
@@ -11,6 +11,20 @@ from wyrmwood_coffee.models.vendor import (
 router = APIRouter()
 
 
+@router.get(
+    "",
+    status_code=status.HTTP_200_OK,
+    response_model=list[VendorRead],
+    response_description="A list of all vendors",
+)
+def list_vendors(session: DbSession) -> list[VendorRead]:
+    """
+    Retrieve a list of all vendors.
+    """
+    vendors = session.query(Vendor).all()
+    return [VendorRead.model_validate(v, from_attributes=True) for v in vendors]
+
+
 @router.post(
     "",
     status_code=status.HTTP_201_CREATED,
